2cc3f1c39ced1fb1dfbac94766a1e66f1a145850
Kalindu Rashmina <krashmina612@gmail.com>
2024-08-29 06:00:35 +0530

    initial commit

---
+++ sd-backend/src/db-config.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const db = process.env.MONGODB_URI;
+
+mongoose.set("strictQuery", true, "useNewUrlParser", true);
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(db);
+    console.log("Database is connected");
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+module.exports = dbConnect;
---
+++ sd-backend/src/routes/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const path = require("path");
+
+const router = express.Router();
+
+// Define a route to serve static files (like images)
+
+module.exports = router;
