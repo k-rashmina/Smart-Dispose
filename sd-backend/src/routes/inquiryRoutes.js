a27a6decc69f3e6fa52296930b38a2c8afc4743e
asirialwis <asirichathurika900@gmail.com>
2024-09-03 19:49:25 +0530

    inquiry update add

--- sd-backend/src/routes/inquiryRoutes.js
+++ sd-backend/src/routes/inquiryRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const createController = require('../controllers/asiri/inquiryCreate');
+const updateController = require('../controllers/asiri/inquiryUpdate');
 
 router.post('/createInquiry', createController);
+router.put('/updateInquiry/:id', updateController);
 
 module.exports = router;
