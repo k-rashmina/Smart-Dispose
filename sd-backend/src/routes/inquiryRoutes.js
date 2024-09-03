07696d0c673967f89ea832076ecee9c06124d099
asirialwis <asirichathurika900@gmail.com>
2024-09-03 20:40:52 +0530

    inq delete add

--- sd-backend/src/routes/inquiryRoutes.js
+++ sd-backend/src/routes/inquiryRoutes.js
@@ -3,8 +3,14 @@ const router = express.Router();
 
 const createController = require('../controllers/asiri/inquiryCreate');
 const updateController = require('../controllers/asiri/inquiryUpdate');
+const getController = require('../controllers/asiri/inquiryGet');
+const getOneController = require('../controllers/asiri/inquiryGetOne');
+const deleteController = require('../controllers/asiri/inquiryDelete');
 
 router.post('/createInquiry', createController);
 router.put('/updateInquiry/:id', updateController);
+router.get('/getInquiry', getController);
+router.get('/getOneInquiry/:id', getOneController);
+router.delete('/deleteInquiry/:id', deleteController);
 
 module.exports = router;
