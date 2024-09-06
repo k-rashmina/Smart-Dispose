7250af9a83d9fb61236c3a4e813de6d950cd1f45
Yohan Christmith Weerapperuma <it22328434@my.sliit.lk>
2024-09-06 11:05:05 +0530

    Add payment routes to index.js

--- sd-backend/src/routes/index.js
+++ sd-backend/src/routes/index.js
@@ -5,11 +5,13 @@ const router = express.Router();
 
 // Define a route to serve static files (like images)
 
-
 const customerRoutes = require("./customerRoutes");
 router.use("/customer", customerRoutes);
 
 const inquiryRoutes = require("./inquiryRoutes");
 router.use("/inquiry", inquiryRoutes);
+
+const paymentRoutes = require("./paymentRoutes");
+router.use("/payments", paymentRoutes);
 
 module.exports = router;
