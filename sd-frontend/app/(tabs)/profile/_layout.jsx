70531e6a5d59424b54c871b0e5ef3d7687ebc0ef
Kalindu Rashmina <krashmina612@gmail.com>
2024-09-18 05:13:46 +0530

    navigation done 3

--- sd-frontend/app/(tabs)/profile/_layout.jsx
+++ sd-frontend/app/(tabs)/profile/_layout.jsx
@@ -5,7 +5,7 @@ import { Stack } from 'expo-router'
 const ProfileLayout = () => {
   return (
     <Stack>
-      <Stack.Screen name='index' options={{headerShown: true, title: 'Payment', headerTitleAlign: 'center'}} />
+      <Stack.Screen name='index' options={{headerShown: true, title: 'Profile', headerTitleAlign: 'center'}} />
     </Stack>
   )
 }
