2069f1f4562e34c0cb688df8943fbd4395b34d61
asirialwis <asirichathurika900@gmail.com>
2024-09-30 15:15:18 +0530

    Report gen backend init

--- sd-backend/src/routes/inquiryRoutes.js
+++ sd-backend/src/routes/inquiryRoutes.js
@@ -6,11 +6,13 @@ const updateController = require('../controllers/asiri/inquiryUpdate');
 const getController = require('../controllers/asiri/inquiryGet');
 const getOneController = require('../controllers/asiri/inquiryGetOne');
 const deleteController = require('../controllers/asiri/inquiryDelete');
+const inquiryReportController = require('../controllers/asiri/inquiryReport');
 
 router.post('/createInquiry', createController);
 router.put('/updateInquiry/:id', updateController);
 router.get('/getInquiry', getController);
 router.get('/getOneInquiry/:id', getOneController);
 router.delete('/deleteInquiry/:id', deleteController);
+router.get('/inquiryReport', inquiryReportController);
 
 module.exports = router;
