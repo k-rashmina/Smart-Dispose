1c9a8f42a5519234efa5e9ef69cce28c29279584
asirialwis <asirichathurika900@gmail.com>
2024-09-30 19:46:22 +0530

    inquiryReport frontend init

---
+++ sd-admin/src/components/inquiryReport.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AdminHeader from "./common/AdminHeader";
+
+function inquiryReport() {
+  return (
+    <div>
+         <AdminHeader pageName={"Inquiry Report"} />
+        inquiryReport</div>
+  )
+}
+
+export default inquiryReport
