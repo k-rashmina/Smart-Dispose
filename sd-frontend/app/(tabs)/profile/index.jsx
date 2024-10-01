83557bcff1663c9461fe4073c22760b5d8ec6ca8
IT22338266 <it22338266@my.sliit.lk>
2024-10-01 20:58:23 +0530

    Refactor Profile component to add logout functionality

--- sd-frontend/app/(tabs)/profile/index.jsx
+++ sd-frontend/app/(tabs)/profile/index.jsx
@@ -1,13 +1,52 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Link } from 'expo-router'
+import { StyleSheet, Text, View, Button, BackHandler, Alert } from 'react-native'
+import React, {useEffect} from 'react'
+import { Link, useRouter } from 'expo-router'
+import { logoutUser } from '../../(auth)/Auth';
 
-const Profile = () => {
+
+
+
+const Profile = () => { 
+
+
+  const router = useRouter();
+
+  // Handle back button press behavior
+//   useEffect(() => {
+//     const backAction = () => {
+//       // When back is pressed, instead of going back to the login page, exit the app
+//       Alert.alert("Hold on!", "Are you sure you want to exit the app?", [
+//         {
+//           text: "Cancel",
+//           onPress: () => null,
+//           style: "cancel",
+//         },
+//         { text: "YES", onPress: () => BackHandler.exitApp() },
+//       ]);
+//       return true; // Prevent default back button behavior
+//     };
+
+//     const backHandler = BackHandler.addEventListener(
+//       "hardwareBackPress",
+//       backAction
+//     );
+
+//     // Cleanup the event listener when component unmounts
+//     return () => backHandler.remove();
+//   }, []);
+
+  const handleLogout = () => {
+    logoutUser ().then(() => {
+      router.push("/login");
+    });
+
+  };
   return (
     <View>
       <Text>Profile</Text>
       <Link style={{marginTop: 50, alignSelf: 'center', color: 'blue', fontSize: 20}} href={'/profile/inqlist'}>Inquiries</Link>
       <Link style={{marginTop: 50, alignSelf: 'center', color: 'blue', fontSize: 20}} href={'/profile/userDetails'}>UserDetails</Link>
+      <Button title="Logout" onPress={handleLogout} />
     </View>
   )
 }
