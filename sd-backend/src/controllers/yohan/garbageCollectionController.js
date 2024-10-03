7dce1e1ff1aa420b4b6326f7d041c5084ce05312
Yohan Christmith Weerapperuma <it22328434@my.sliit.lk>
2024-10-03 13:11:19 +0530

    Refactor garbageCollectionController.js to allow fetching garbage collection records by email or email with date range

--- sd-backend/src/controllers/yohan/garbageCollectionController.js
+++ sd-backend/src/controllers/yohan/garbageCollectionController.js
@@ -16,8 +16,31 @@ exports.createGarbageCollection = async (req, res) => {
 exports.getGarbageCollectionRecordsByEmail = async (req, res) => {
   try {
     const email = req.params.email;
-    const garbageCollectionRecords =
-      await GarbageCollectionService.getGarbageCollectionRecordsByEmail(email);
+    const { startDate, endDate } = req.query;
+
+    let garbageCollectionRecords;
+
+    if (startDate && endDate) {
+      const start = new Date(startDate);
+      const end = new Date(endDate);
+
+      // Validate date objects
+      if (isNaN(start) || isNaN(end)) {
+        return res.status(400).json({ error: "Invalid date format" });
+      }
+
+      garbageCollectionRecords =
+        await GarbageCollectionService.getGarbageCollectionByDateRange(
+          email,
+          start,
+          end
+        );
+    } else {
+      garbageCollectionRecords =
+        await GarbageCollectionService.getGarbageCollectionRecordsByEmail(
+          email
+        );
+    }
     res.status(200).json(garbageCollectionRecords);
   } catch (err) {
     res.status(500).json({ error: err.message });
