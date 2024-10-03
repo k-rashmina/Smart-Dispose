5de7e0ebbf455c20a302a1937a96b0113864ef26
Yohan Christmith Weerapperuma <it22328434@my.sliit.lk>
2024-10-03 13:11:28 +0530

    Refactor garbageCollectionDataAccess.js to add method for fetching garbage collection records by date range

--- sd-backend/src/data-access/yohan/garbageCollectionDataAccess.js
+++ sd-backend/src/data-access/yohan/garbageCollectionDataAccess.js
@@ -12,6 +12,17 @@ class GarbageCollectionDataAccess {
     return await garbageCollectionModel.find({ email });
   }
 
+  //function for getting garbage collection records by email and range of dates
+  async getGarbageCollectionByDateRange(email, startDate, endDate) {
+    return await garbageCollectionModel.find({
+      email,
+      collectionDate: {
+        $gte: startDate,
+        $lte: endDate,
+      },
+    });
+  }
+
   //function for getting all garbage collection records
   async getAllGarbageCollectionRecords() {
     return await garbageCollectionModel.find();
