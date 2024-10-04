77a5c4f5d774c083debb617b7e175ebe8a065888
Yohan Christmith Weerapperuma <it22328434@my.sliit.lk>
2024-10-04 17:10:45 +0530

    Refactor app.js to add monthly bill generation scheduling

--- sd-backend/src/app.js
+++ sd-backend/src/app.js
@@ -4,12 +4,13 @@ const dotenv = require("dotenv");
 const dbConnect = require("./db-config");
 const routes = require("./routes");
 const monitorBinLevelService = require("./services/kalindu/monitorBinLevelService");
+const MonthlyBillService = require("./services/yohan/monthlyBillService");
 
 //initialization and middleware
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 5000;
-app.use(cors(['http://localhost:5173/*']));
+app.use(cors(["http://localhost:5173/*"]));
 app.use(express.json({ limit: "15mb" }));
 //routes
 app.use("/", routes);
@@ -24,4 +25,7 @@ app.listen(port, () => {
   console.log(`listening on port ${port}`);
   dbConnect();
   monitorBinLevelService();
+
+  // Schedule monthly bill generation
+  MonthlyBillService.scheduleMonthlyBillGeneration();
 });
